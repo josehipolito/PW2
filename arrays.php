202918da195589dad99a5c93f58b9372a2bf1f74
Joao Hipolito <joaopedrohipolito.2004@gmail.com>
2025-11-20 17:21:24 +0000

    update

--- arrays.php
+++ arrays.php
@@ -3,10 +3,16 @@
 // FUNÇÃO DE CONEXÃO PDO
 // ----------------------------
 function conectarBD() {
-    $host = 'localhost';
+ /*   $host = 'localhost';
     $db   = 'premier_league';
     $user = 'pw2';
     $pass = '1234';
+    $charset = 'utf8mb4';*/
+
+    $host = 'localhost';
+    $db   = 'u506280443_josjoaDB';
+    $user = 'u506280443_josjoadbUser';
+    $pass = '7$&9N~8XpT';
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
